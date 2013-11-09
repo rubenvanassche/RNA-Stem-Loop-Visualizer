f7e4d75691b159d0018c21967d4c764a1524fdc6
Stijn-Flipper <w.stijn@gmail.com>
2013-11-10 00:15:17 +0100

    added main source file and main test

---
+++ src/Tests.cpp
@@ -0,0 +1,30 @@
+/* Tests
+ * Wrapper to run ALL the tests.
+ *
+ * Copyright (C) 2013 Stijn Wouters
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Last modified:  9 November 2013
+ * By: Stijn Wouters
+ */
+
+/*
+ * In order to make it work properly, be sure the macro below does NOT
+ * appear in any of the source files. Also be sure that there's no main
+ * section there.
+ */
+#define CATCH_CONFIG_MAIN
+
+#include "Catch.h"
