e442e955939fd7d00d7681086202d0579a656aae
Stijn-Flipper <w.stijn@gmail.com>
2013-11-11 21:02:42 +0100

    added license

--- src/CFG.h
+++ src/CFG.h
@@ -1,8 +1,23 @@
 /*
  * CFG.h
  *
- *  Created on: 8 nov. 2013
- *      Author: Ruben
+ * Copyright (C) 2013 Ruben Van Assche
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Last modified: 8 November 2013
+ * By: Ruben Van Assche
  */
 
 #ifndef CFG_H_
@@ -15,14 +30,14 @@
 /**
  * @brief Class representing a sequence of terminals and variables
  */
-class symbolString{
+class SymbolString{
 public:
 	 /**
 	 * @brief Constructor
 	 *
 	 * @param string A string containing variables and symbols
 	 */
-	symbolString(std::string string);
+	SymbolString(std::string string);
 
 	 /**
 	 * @brief Get the string
--- src/PDA.cpp
+++ src/PDA.cpp
@@ -1,8 +1,23 @@
 /*
  * PDA.cpp
  *
- *  Created on: 8 nov. 2013
- *      Author: Ruben
+ * Copyright (C) 2013 Ruben Van Assche
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Last modified: 8 November 2013
+ * By: Ruben Van Assche*
  */
 
 #include "PDA.h"
