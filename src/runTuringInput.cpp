fff748af36636cfc9e3f16caed50abb1f62a40e8
JakobStruye <jakob.struye@hotmail.com>
2013-12-09 18:28:37 +0000

    Added Turing Machine manual test executable for workshop. Added LL Parser to CMake file list. Temporarily removed warning flags (other teams in the workshop shouldn't see them)

---
+++ src/runTuringInput.cpp
@@ -0,0 +1,37 @@
+#include "Turing.h"
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cout << "Please provide the name of an xml file describing a turing machine as command line argument!" << std::endl;
+        return 0;
+    }
+    TuringMachine* TM;
+
+    try {
+        TM = generateTM(argv[1]);
+    }
+    catch (std::runtime_error& e) {
+        std::cout << e.what() << std::endl;
+        return 0;
+    }
+    while (true) {
+        std::cout << "Please enter a string to be processed by TM (type 'quit' to quit program): " << std::endl;;
+        std::string input;
+        std::getline(std::cin, input);
+        bool answer;
+        if (input == "quit")
+            return 0;
+        try {
+            answer = TM->process(input);
+        }
+        catch (std::runtime_error& e) {
+            std::cout << e.what() << std::endl;
+            continue;
+        }
+        if (answer)
+            std::cout << "String accepted!" << std::endl;
+        else
+            std::cout << "String NOT accepted!" << std::endl;
+    }
+}
