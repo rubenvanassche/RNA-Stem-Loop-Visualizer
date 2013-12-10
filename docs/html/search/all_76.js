cda854c4b1b8f57227bb88f2f0932bbb1bd041dc
Stijn-Flipper <w.stijn@gmail.com>
2013-12-10 18:38:32 +0100

    only a doxyfile instead a bunch of craps from doxygen + also working on any computer

--- docs/html/search/all_76.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['valuesgenerator',['ValuesGenerator',['../class_catch_1_1_values_generator.html',1,'Catch']]],
-  ['verbosity',['Verbosity',['../struct_catch_1_1_verbosity.html',1,'Catch']]]
-];
--- docs/html/search/functions_7e.js
+++
@@ -1,9 +0,0 @@
-var searchData=
-[
-  ['_7ecfg',['~CFG',['../class_c_f_g.html#a7a9e0feb406099038faea4c67ef31786',1,'CFG']]],
-  ['_7ellparser',['~LLParser',['../class_l_l_parser.html#afcc3d3e47b886797f852c50c705ffb5d',1,'LLParser']]],
-  ['_7elltable',['~LLTable',['../class_l_l_table.html#a37eed818542510ffc9ce790d2c7e7e84',1,'LLTable']]],
-  ['_7eturingmachine',['~TuringMachine',['../class_turing_machine.html#a5279d180cd87e664447c6b40e0ce55e2',1,'TuringMachine']]],
-  ['_7eturingstate',['~TuringState',['../class_turing_state.html#ae5613f5e5d5e669ad1e56c772b1b8896',1,'TuringState']]],
-  ['_7eturingtransition',['~TuringTransition',['../class_turing_transition.html#a864a90c01b40bfa7cdcf545980d0f546',1,'TuringTransition']]]
-];
