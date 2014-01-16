9983b6d096e4538f31dcf5632e5e7c585338d87d
Ruben Van Assche <rubenvanassche@gmail.com>
2014-01-16 15:13:53 +0100

    GUI updates

--- src/UI/mainwindow.cpp
+++ src/UI/mainwindow.cpp
@@ -1,12 +1,11 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-#include <QMessageBox>
-
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow){
     ui->setupUi(this);
+    ui->VisualizeButton->setDisabled(true);
 }
 
 MainWindow::~MainWindow()
@@ -15,17 +14,28 @@ MainWindow::~MainWindow()
 }
 
 void MainWindow::on_AnalyzeButton_clicked(){
+    ui->VisualizeButton->setDisabled(true);
+    ui->AnalyzeButton->setDisabled(true);
+    ui->AnalyzeButton->setText("Working ...");
+    this->fVisualizerLoop = "";
+
     QString loop = ui->Input->toPlainText();
     if(loop.isEmpty()){
         QMessageBox::information(NULL, "Problem", "Please provide A RSL.");
+
+        ui->AnalyzeButton->setEnabled(true);
+        ui->AnalyzeButton->setText("Analyze");
+        return;
     }
 
-    ui->AnalyzeButton->setDisabled(true);
-    ui->AnalyzeButton->setText("Working ...");
-    ui->OpenAlgoWindowButton->setDisabled(true);
-
     const QString algoType =  ui->AnalyzeTypeSelect->currentText();
-    if(algoType == "PDA"){
+
+    std::string RNALoop = loop.toStdString(); // Work with this to start your algorithm
+    bool accepted = true; // Change this to true if the loop is accepted
+    std::string visualizerLoop = ""; // Store the loop for visualizing here
+    // That's it!
+    if(algoType == "CFG"){
+
 
     }else if(algoType == "LLParser"){
 
@@ -35,15 +45,20 @@ void MainWindow::on_AnalyzeButton_clicked(){
         QMessageBox::information(NULL, "Problem", "This algorithm for processing the RSL is not (yet avaible).");
     }
 
+    if(accepted == true){
+    	ui->VisualizeButton->setDisabled(false);
+    	QMessageBox::information(NULL, "Accepted", "This loop was accepted.");
+    	this->fVisualizerLoop = visualizerLoop;
+    }else{
+    	QMessageBox::information(NULL, "Not Accepted", "This loop was not accepted.");
+    }
+
     ui->AnalyzeButton->setEnabled(true);
     ui->AnalyzeButton->setText("Analyze");
-    ui->OpenAlgoWindowButton->setEnabled(true);
 }
 
-
-
-void MainWindow::on_OpenAlgoWindowButton_clicked()
-{
-    AlgoTest* algow = new AlgoTest(); // Be sure to destroy you window somewhere
-    algow->show();
+void MainWindow::on_VisualizeButton_clicked(){
+	std::string command = " ./RNA-Stem-Loop-Visualizer '" + this->fVisualizerLoop + "'";
+	system(command.c_str());
 }
+
