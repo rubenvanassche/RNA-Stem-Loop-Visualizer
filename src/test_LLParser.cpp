4686ebee0799b13f2dd7aec0671524f2164b3511
Pieter Lauwers <lauwers.pieter@gmail.com>
2014-01-16 21:31:00 +0100

    Remove empty function

--- src/test_LLParser.cpp
+++ src/test_LLParser.cpp
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * Last modified: 13 December 2013
+ * Last modified: 16 January 2013
  * By: Pieter Lauwers
  */
 
